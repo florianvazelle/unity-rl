649f45910e7dc026847a7b173273ca7995bb963d
Antoine <antoine.thenevin@gmail.com>
2021-03-10 16:57:36 +0100

    Pseudo code for Monte Carlo

---
+++ Assets/Scripts/Algo/Monte Carlo/Monte Carlo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonteCarlo : MonoBehaviour
+{
+    // Pi(s) => dictionnaire de Istate (clef = Istate, value = int "action associée")
+        // -> initialisé à random
+    // States
+
+    // Q(s,a) => Dictionary<(IState, int), int> (on stocke la moyenne des returns)
+        // -> initialisé à random
+
+    // Returns(s, a) 
+        // -> initialiser à vide
+    
+
+    private void Start()
+    {
+        // Pour chaque état, initialiser la politique avec une action random
+            // (Comme bcp de possibilité, un seul élément dans le dictionnaire)
+    }
+
+    // Loop forever (for each episode or game)
+        // start from a random state
+
+        // creation d'une liste de (State Action Reward)
+
+        // generate random game until the end
+            // apply transition method on S0 & A0
+            // add An, Sn, Rn à la liste 
+
+        // G = 0
+        // Loop for each step (on remonte les infos qu'on a obtenue (reward)) (on commence par l'avant derniére étape)
+            // G = gamma * G + R(t+1) (reward de l'itération suivante) (on va de la fin au debut) (la derniére en premmier)
+
+            // Si on est pas deja passé par cet état
+                // on ajoute G à Returns(s, a)
+
+                // on ajoute la moyenne des Returns(s, a) dans Q(s,a)
+                // pour toute les actions possibles de l'état on ajoute l'action qui a la meilleure moyenne
+            
+            
+            
+}
